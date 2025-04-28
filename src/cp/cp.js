f71fb69c610e398d76e1413bba53620c0a59e0b2
MrBorker <anastasiakondratyushechka@gmail.com>
2025-04-28 22:55:19 +0200

    feat: worker threads and child process

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,30 @@
+import { spawn } from "child_process";
+
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn(
+    "node",
+    [resolve(__dirname, "files", "script.js"), ...args],
+    {
+      stdio: ["pipe", "pipe", "pipe"],
+    }
+  );
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.pipe(process.stdout);
+
+  child.stderr.pipe(process.stderr);
+
+  child.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["someArgument1", "someArgument2", "dog", "cat"]);
--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,46 @@
+import { Worker } from "worker_threads";
+import os from "os";
+
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  const numWorkers = os.cpus().length;
+  const results = [];
+  let completedWorkers = 0;
+
+  for (let i = 0; i < numWorkers; i++) {
+    const worker = new Worker(resolve(__dirname, "worker.js"));
+
+    worker.postMessage(10 + i);
+
+    worker.on("message", (message) => {
+      results[i] = message;
+      completedWorkers++;
+
+      if (completedWorkers === numWorkers) {
+        console.log("All workers finished:", results);
+      }
+    });
+
+    worker.on("error", (error) => {
+      results[i] = { status: "error", data: null };
+      completedWorkers++;
+
+      if (completedWorkers === numWorkers) {
+        console.log("All workers finished:", results);
+      }
+    });
+
+    worker.on("exit", (code) => {
+      if (code !== 0) {
+        console.error(`Worker stopped with exit code ${code}`);
+      }
+    });
+  }
 };
 
 await performCalculations();
